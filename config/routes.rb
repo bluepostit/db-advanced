de5fa5f96a7ee3df79563af4ca10b0d0db01fe43
Yair <32864499+bluepostit@users.noreply.github.com>
2021-11-25 14:14:33 +0100

    sharing data with team members

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
+  authenticate :user, ->(user) { user.admin? } do
+    mount Blazer::Engine, at: "blazer"
+  end
+
   root to: 'pages#home'
 end
